1efdbc51f5859b8d57c7651f76179b4a536dab5d
undg <und3rdg@gmail.com>
2025-06-08 11:38:34 +0100

    add comment with available voices

--- api.py
+++ api.py
@@ -18,6 +18,38 @@ class SynthesizeRequest(BaseModel):
     voice: str = "af_heart"
 
 
+# CHOICES = {
+# '🇺🇸 🚺 Heart ❤️': 'af_heart',
+# '🇺🇸 🚺 Bella 🔥': 'af_bella',
+# '🇺🇸 🚺 Nicole 🎧': 'af_nicole',
+# '🇺🇸 🚺 Aoede': 'af_aoede',
+# '🇺🇸 🚺 Kore': 'af_kore',
+# '🇺🇸 🚺 Sarah': 'af_sarah',
+# '🇺🇸 🚺 Nova': 'af_nova',
+# '🇺🇸 🚺 Sky': 'af_sky',
+# '🇺🇸 🚺 Alloy': 'af_alloy',
+# '🇺🇸 🚺 Jessica': 'af_jessica',
+# '🇺🇸 🚺 River': 'af_river',
+# '🇺🇸 🚹 Michael': 'am_michael',
+# '🇺🇸 🚹 Fenrir': 'am_fenrir',
+# '🇺🇸 🚹 Puck': 'am_puck',
+# '🇺🇸 🚹 Echo': 'am_echo',
+# '🇺🇸 🚹 Eric': 'am_eric',
+# '🇺🇸 🚹 Liam': 'am_liam',
+# '🇺🇸 🚹 Onyx': 'am_onyx',
+# '🇺🇸 🚹 Santa': 'am_santa',
+# '🇺🇸 🚹 Adam': 'am_adam',
+# '🇬🇧 🚺 Emma': 'bf_emma',
+# '🇬🇧 🚺 Isabella': 'bf_isabella',
+# '🇬🇧 🚺 Alice': 'bf_alice',
+# '🇬🇧 🚺 Lily': 'bf_lily',
+# '🇬🇧 🚹 George': 'bm_george',
+# '🇬🇧 🚹 Fable': 'bm_fable',
+# '🇬🇧 🚹 Lewis': 'bm_lewis',
+# '🇬🇧 🚹 Daniel': 'bm_daniel',
+# }
+
+
 @api.post("/tts")
 async def tts(text: str = Form(...), voice: str = Form("af_heart")):
     try:
